372ddf1eb54d9a0398829f470a8816f03cf1573b
jooYeon <jooyeonstudyios@gmail.com>
2024-07-24 05:38:30 +0900

    [design]: 텍스트필드 10자 제한 추가

--- POPO/View/Onboarding/OnboardingView.swift
+++ POPO/View/Onboarding/OnboardingView.swift
@@ -8,19 +8,19 @@
 import UIKit
 import SnapKit
 
-class OnboardingView: BaseView, UIScrollViewDelegate {
-
+class OnboardingView: BaseView {
+    
     lazy var scrollView = UIScrollView()
     lazy var pageControl = UIPageControl()
     
     lazy var namePageView = UIView()
     lazy var agePageView = UIView()
-
+        
     override func configure() {
         super.configure()
         backgroundColor = .userLightGreen
     }
-
+    
     override func setupUI() {
         scrollView.isPagingEnabled = true
         scrollView.showsHorizontalScrollIndicator = false
@@ -44,22 +44,23 @@ class OnboardingView: BaseView, UIScrollViewDelegate {
         
         scrollView.addSubviews([namePageView, agePageView])
     }
-
+    
     private func setupNamePageView() {
         let nameLabel = CustomLabel(text: "행운이 찾아갈 너의 이름을 알려줘!", font: .point(ofSize: 15))
         
         let nameTextField = UITextField()
+        nameTextField.delegate = self
         nameTextField.background = UIImage(named: "BlurButton")
         nameTextField.textAlignment = .center
         nameTextField.tintColor = .userGreen
-
+        
         let placeholderAttributes: [NSAttributedString.Key: Any] = [
             .font: UIFont.body(ofSize: 13),
             .foregroundColor: UIColor.userGray(4)
         ]
         nameTextField.attributedPlaceholder = NSAttributedString(string: "닉네임, 애칭, 본명 뭐든 좋아",
                                                                  attributes: placeholderAttributes)
-               
+        
         
         namePageView.addSubviews([nameLabel, nameTextField])
         
@@ -77,7 +78,7 @@ class OnboardingView: BaseView, UIScrollViewDelegate {
     
     private func setupAgePageView() {
         let ageLabel = CustomLabel(text: "나이대도 알려줄래?", font: .point(ofSize: 15))
-    
+        
         agePageView.addSubviews([ageLabel])
         
         ageLabel.snp.makeConstraints { make in
@@ -85,7 +86,7 @@ class OnboardingView: BaseView, UIScrollViewDelegate {
             make.centerX.equalToSuperview()
         }
     }
-
+    
     override func setupLayout() {
         scrollView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
@@ -112,9 +113,21 @@ class OnboardingView: BaseView, UIScrollViewDelegate {
             make.leading.equalTo(namePageView.snp.trailing)
         }
     }
+}
+
+extension OnboardingView: UIScrollViewDelegate {
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let pageIndex = round(scrollView.contentOffset.x / frame.width)
         pageControl.currentPage = Int(pageIndex)
+    }
+}
+
+extension OnboardingView: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        guard let currentText = textField.text else { return true }
+        let newLength = currentText.count + string.count - range.length
+        return newLength <= 10
     }
 }
