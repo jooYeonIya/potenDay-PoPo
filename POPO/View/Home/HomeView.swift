a0b9695911b2af9a27f49f32e273563e6ffee49e
jooyeonIya <jooyeonstudyios@gmail.com>
2024-07-24 11:33:33 +0900

    [design]: Home 배경 화면 추가

--- POPO/View/Home/HomeView.swift
+++ POPO/View/Home/HomeView.swift
@@ -8,7 +8,47 @@
 import UIKit
 
 class HomeView: BaseView {
+    lazy var pinkCircleView = UIView()
+    lazy var greenCircleView = UIView()
+    
+    lazy var viewBlurEffect = UIVisualEffectView()
+
     override func configure() {
-        backgroundColor = .white
+        super.configure()
+        backgroundColor = .userGray(9)
+    }
+    
+    override func setupUI() {
+        addSubviews([pinkCircleView, greenCircleView, viewBlurEffect])
+        
+        viewBlurEffect.effect = UIBlurEffect(style: .light)
+        
+        applyCircleViews()
+    }
+    
+    private func applyCircleViews() {
+        pinkCircleView.layer.cornerRadius = 411 / 2
+        pinkCircleView.backgroundColor = .userPink
+        
+        greenCircleView.layer.cornerRadius = 411 / 2
+        greenCircleView.backgroundColor = .userLightGreen
+    }
+    
+    override func setupLayout() {
+        viewBlurEffect.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        pinkCircleView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide.snp.top).offset(12)
+            make.leading.equalToSuperview().offset(100)
+            make.width.height.equalTo(411)
+        }
+        
+        greenCircleView.snp.makeConstraints { make in
+            make.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-12)
+            make.trailing.equalToSuperview().offset(-100)
+            make.width.height.equalTo(411)
+        }
     }
 }
