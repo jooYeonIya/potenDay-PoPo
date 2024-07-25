325e081a459cc4dbe8edb26ea7f98e0573c1e734
jooyeonIya <jooyeonstudyios@gmail.com>
2024-07-25 14:00:25 +0900

    [design]: blurview 추가

--- POPO/View/MyPage/MyPageView.swift
+++ POPO/View/MyPage/MyPageView.swift
@@ -11,20 +11,36 @@ class MyPageView: BaseView {
     
     lazy var titleView = TitleView(title: "마이페이지")
     lazy var myInfoView = MyInfoView()
+    lazy var topBackgroundView = UIView()
+    lazy var blurView = UIVisualEffectView()
     
     override func configure() {
         super.configure()
-        backgroundColor = .userGray(9)
+        backgroundColor = .userLightGreen
     }
     
     override func setupUI() {
-        addSubviews([titleView, myInfoView])
+        addSubviews([topBackgroundView, blurView, titleView, myInfoView])
+        
+        topBackgroundView.backgroundColor = .userGray(9)
+
+        blurView.effect = UIBlurEffect(style: .extraLight)
         
         myInfoView.nickname = "나는야럭키걸"
         myInfoView.configure()
     }
     
     override func setupLayout() {
+        topBackgroundView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(myInfoView.snp.bottom).offset(12)
+        }
+
+        blurView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(myInfoView.snp.bottom).offset(80)
+        }
+
         titleView.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide)
             make.leading.trailing.equalToSuperview()
