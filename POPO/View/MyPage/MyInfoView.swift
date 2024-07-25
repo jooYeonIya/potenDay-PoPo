2dbf9b49f65d5762b3806b4adefc4bd294535375
jooyeonIya <jooyeonstudyios@gmail.com>
2024-07-25 13:30:42 +0900

    [design]: MyInfoView 추가

---
+++ POPO/View/MyPage/MyInfoView.swift
@@ -0,0 +1,59 @@
+//
+//  MyInfoView.swift
+//  POPO
+//
+//  Created by 강주연 on 7/25/24.
+//
+
+import UIKit
+
+class MyInfoView: BaseView {
+    lazy var imageView = UIImageView()
+    lazy var titleLabel = CustomLabel(text: "행운의 주인공", font: .body(ofSize: 15))
+    lazy var nicknameTextField = UITextField()
+    lazy var modifyButton = UIButton()
+    
+    var nickname = ""
+
+    override func configure() {
+        super.configure()
+    }
+    
+    override func setupUI() {
+        addSubviews([imageView, titleLabel, nicknameTextField, modifyButton])
+        
+        backgroundColor = .white
+        layer.cornerRadius = 16
+        
+        imageView.image = UIImage(named: "MyInfoImage")
+        
+        nicknameTextField.text = nickname
+        nicknameTextField.isUserInteractionEnabled = false
+        nicknameTextField.font = .bodyBold(ofSize: 22)
+        
+        modifyButton.setImage(UIImage(named: "ModifyUserInfo_Unselected"), for: .normal)
+        modifyButton.setImage(UIImage(named: "ModifyUserInfo_Selected"), for: .selected)
+    }
+    
+    override func setupLayout() {
+        imageView.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview().inset(8)
+            make.width.height.equalTo(56)
+        }
+        
+        titleLabel.snp.makeConstraints { make in            make.leading.equalTo(imageView.snp.trailing).offset(8)
+        }
+        
+        nicknameTextField.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.bottom.equalToSuperview().offset(-16)
+            make.leading.equalTo(titleLabel)
+        }
+        
+        modifyButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().offset(-16)
+            make.width.height.equalTo(24)
+        }
+    }
+}
--- POPO/View/MyPage/MyPageView.swift
+++ POPO/View/MyPage/MyPageView.swift
@@ -10,14 +10,18 @@ import UIKit
 class MyPageView: BaseView {
     
     lazy var titleView = TitleView(title: "마이페이지")
+    lazy var myInfoView = MyInfoView()
     
     override func configure() {
         super.configure()
-        backgroundColor = .white
+        backgroundColor = .userGray(9)
     }
     
     override func setupUI() {
-        addSubviews([titleView])
+        addSubviews([titleView, myInfoView])
+        
+        myInfoView.nickname = "나는야럭키걸"
+        myInfoView.configure()
     }
     
     override func setupLayout() {
@@ -25,6 +29,12 @@ class MyPageView: BaseView {
             make.top.equalTo(safeAreaLayoutGuide)
             make.leading.trailing.equalToSuperview()
             make.height.equalTo(80)
+        }
+        
+        myInfoView.snp.makeConstraints { make in
+            make.top.equalTo(titleView.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(76)
         }
     }
 }
