cb23e339179bfc599000aa44216d5a2f42c0333f
jooYeon <jooyeonstudyios@gmail.com>
2024-07-26 23:15:57 +0900

    [feat]: ChramImage 모델 추가

--- POPO/Application/AppDelegate.swift
+++ POPO/Application/AppDelegate.swift
@@ -10,9 +10,7 @@ import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         IQKeyboardManager.shared.enable = true
         IQKeyboardManager.shared.enableAutoToolbar = false
---
+++ POPO/Model/CharmImage.swift
@@ -0,0 +1,66 @@
+//
+//  CharmImage.swift
+//  POPO
+//
+//  Created by 강주연 on 7/26/24.
+//
+
+import Foundation
+
+struct Coordinates {
+    let x: Int
+    let y: Int
+}
+
+struct ImageCoordinates {
+    let coord1: Coordinates
+    let coord2: Coordinates
+}
+
+enum Images: String {
+    case image1
+    case image2
+    case image3
+    case image4
+    case image5
+    case image6
+    case image7
+    
+    var name: String {
+        switch self {
+        case .image1: return "CharmVer1"
+        case .image2: return "CharmVer2"
+        case .image3: return "CharmVer3"
+        case .image4: return "CharmVer4"
+        case .image5: return "CharmVer5"
+        case .image6: return "CharmVer6"
+        case .image7: return "CharmVer7"
+        }
+    }
+    
+    func coordinates() -> ImageCoordinates {
+        switch self {
+        case .image1:
+            return ImageCoordinates(coord1: Coordinates(x: 180, y: 737),
+                                    coord2: Coordinates(x: 173, y: 1564))
+        case .image2:
+            return ImageCoordinates(coord1: Coordinates(x: 203, y: 354),
+                                    coord2: Coordinates(x: 187, y: 1180))
+        case .image3:
+            return ImageCoordinates(coord1: Coordinates(x: 146, y: 458),
+                                    coord2: Coordinates(x: 244, y: 928))
+        case .image4:
+            return ImageCoordinates(coord1: Coordinates(x: 213, y: 297),
+                                    coord2: Coordinates(x: 186, y: 681))
+        case .image5:
+            return ImageCoordinates(coord1: Coordinates(x: 116, y: 525),
+                                    coord2: Coordinates(x: 120, y: 912))
+        case .image6:
+            return ImageCoordinates(coord1: Coordinates(x: 256, y: 381),
+                                    coord2: Coordinates(x: 152, y: 798))
+        case .image7:
+            return ImageCoordinates(coord1: Coordinates(x: 124, y: 350),
+                                    coord2: Coordinates(x: 147, y: 818))
+        }
+    }
+}
