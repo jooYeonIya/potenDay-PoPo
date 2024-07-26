bb7da3cdd7ecdc8a730792ebcd17f89997120395
jooYeon <jooyeonstudyios@gmail.com>
2024-07-27 00:49:15 +0900

    [feat]: 사진 합성해서 저장하기 기능

---
+++ POPO/Model/CharmReponse.swift
@@ -0,0 +1,24 @@
+//
+//  CharmReponse.swift
+//  POPO
+//
+//  Created by Jooyeon Kang on 7/26/24.
+//
+
+import Foundation
+
+struct CharmReponse: Codable {
+    let data: CharmData
+    let code: Int
+    let message: String
+}
+
+// MARK: - DataClass
+struct CharmData: Codable {
+    let fourIdioms, message: String
+
+    enum CodingKeys: String, CodingKey {
+        case fourIdioms = "four_idioms"
+        case message
+    }
+}
